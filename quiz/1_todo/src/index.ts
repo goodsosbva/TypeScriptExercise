636856aec17d87a2f00b449d31dddf24ef83c5ad
kwon <goodsosbva@naver.com>
2023-08-01 17:42:48 +0900

    tc study start

--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -1,46 +1,61 @@
-let todoItems;
+// 타입이 반복되는 부분을 인터페이스를 이용해서 줄일 수 있다.
+interface Todo {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
+let todoItems: Todo[];
 
 // api
 function fetchTodoItems() {
   const todos = [
-    { id: 1, title: '안녕', done: false },
-    { id: 2, title: '타입', done: false },
-    { id: 3, title: '스크립트', done: false },
+    { id: 1, title: "안녕", done: false },
+    { id: 2, title: "타입", done: false },
+    { id: 3, title: "스크립트", done: false },
   ];
   return todos;
 }
 
 // crud methods
-function fetchTodos() {
+function fetchTodos(): Todo[] {
   const todos = fetchTodoItems();
   return todos;
 }
 
-function addTodo(todo) {
+function addTodo(todo: { id: number; title: string; done: boolean }): void {
   todoItems.push(todo);
 }
 
-function deleteTodo(index) {
+function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index, todo) {
+function completeTodo(
+  index: number,
+  todo: { id: number; title: string; done: boolean }
+): void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
 
 // business logic
-function logFirstTodo() {
+function logFirstTodo(): { id: number; title: string; done: boolean } {
   return todoItems[0];
 }
 
 function showCompleted() {
-  return todoItems.filter(item => item.done);
+  return todoItems.filter(
+    (item: { id: number; title: string; done: boolean }) => item.done
+  );
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
 function addTwoTodoItems() {
   // addTodo() 함수를 두 번 호출하여 todoItems에 새 할 일이 2개 추가되어야 합니다.
+  const id = todoItems.length;
+  addTodo({ id: id + 1, title: "test", done: false });
+  addTodo({ id: id + 2, title: "test", done: false });
 }
 
 // NOTE: 유틸 함수
@@ -51,3 +66,5 @@ function log() {
 todoItems = fetchTodoItems();
 addTwoTodoItems();
 log();
+console.log(todoItems);
+console.log("end!");
